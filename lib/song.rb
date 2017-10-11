0210f2300a1c0b66d45e185e4f9cbf4328ec10e9
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-11 15:04:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 class Song
+  extend Memorable
   attr_accessor :name
   attr_reader :artist
 
@@ -18,9 +19,9 @@ class Song
     @artist = artist
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
   def to_param
     name.downcase.gsub(' ', '-')
