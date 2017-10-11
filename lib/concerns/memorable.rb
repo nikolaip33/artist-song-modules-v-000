9fc1ae7b793b606f8782b0463cdd211af3f717a3
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-11 16:15:46 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,13 @@
 module Memorable
-  def reset_all
-    self.all.clear
-  end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def count
-    self.all.count
+    def count
+      self.all.count
+    end
   end
+  module InstanceMethods
+  end #module InstanceMethods
 end #module Memorable
