ad6e6af53e69f60dca8afd4392e9bb071adcb1ce
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-11 15:06:55 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,14 +19,6 @@ class Artist
   def self.all
     @@artists
   end
-  #
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   @@artists.count
-  # end
 
   def add_song(song)
     @songs << song
